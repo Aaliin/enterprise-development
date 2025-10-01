e72e74bf668b9fd146f8cb389df509883a5f52d7
Alina <ashavarina07@gmail.com>
2025-10-01 12:13:19 +0400

    Убрала лишние using и небольшие изменения

--- EstateAgency.Domain/Enum/RequestType.cs
+++ EstateAgency.Domain/Enum/RequestType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EstateAgency.Domain.Enum;
+﻿namespace EstateAgency.Domain.Enum;
 
 /// <summary>
 /// Тип заявки от клиента
