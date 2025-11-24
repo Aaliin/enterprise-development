0d529e1c67b3c4f1c5c093e9a103651f100024d4
Alina <ashavarina07@gmail.com>
2025-11-24 13:37:18 +0400

    Реализовано управление приложением

---
+++ EstateAgency.AppHost/Program.cs
@@ -0,0 +1,10 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var sql = builder.AddSqlServer("sql")
+    .AddDatabase("estateagencydb");
+
+builder.AddProject<Projects.EstateAgency_API>("estateagency-api")
+    .WithReference(sql)
+    .WithEnvironment("UseEF", "true");
+
+builder.Build().Run();
